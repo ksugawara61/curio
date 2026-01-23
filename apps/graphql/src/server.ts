a7b487ba5f02c53f3cb0261b33b4b1b7f207d6e4
k2o <11086104+ksugawara61@users.noreply.github.com>
2026-01-23 23:20:49 +0900

    feat: graphql

--- apps/graphql/src/server.ts
+++ apps/graphql/src/server.ts
@@ -1,14 +1,18 @@
+import fs from "node:fs";
 import http from "node:http";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { ApolloServer } from "@apollo/server";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import express from "express";
 
-// The GraphQL schema
-const typeDefs = `#graphql
-  type Query {
-    hello: String
-  }
-`;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const typeDefs = fs.readFileSync(
+  path.join(__dirname, "schema.graphql"),
+  "utf-8"
+);
 
 // A map of functions which return data for the schema.
 const resolvers = {

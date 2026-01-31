db2c5d56c11a8c743d9dd49aa3f764b2f56ec401
k2o <11086104+ksugawara61@users.noreply.github.com>
2026-01-31 13:55:22 +0900

    fix: turso settings

--- apps/graphql/drizzle.config.ts
+++ apps/graphql/drizzle.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./src/libs/drizzle/schema.ts",
   out: "./src/libs/drizzle/migrations",
-  dialect: "sqlite",
+  dialect: "turso",
   dbCredentials: {
     url: process.env.TURSO_DATABASE_URL || "",
     authToken: process.env.TURSO_AUTH_TOKEN || "",

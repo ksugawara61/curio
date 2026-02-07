1d22e7b88d1a64fae214222072c4ef4eaac6f5ee
k2o <11086104+ksugawara61@users.noreply.github.com>
2026-02-07 21:48:00 +0900

    feat: github provider で better-auth の処理を追加

--- apps/graphql/pylon.d.ts
+++ apps/graphql/pylon.d.ts
@@ -6,6 +6,9 @@ declare module "@getcronit/pylon" {
     TURSO_DATABASE_URL: string;
     TURSO_AUTH_TOKEN: string;
     CLERK_SECRET_KEY: string;
+    BETTER_AUTH_BASE_URL: string;
+    BETTER_AUTH_CLIENT_ID: string;
+    BETTER_AUTH_CLIENT_SECRET: string;
     NODE_ENV?: string;
     TEST_AUTH_KEY?: string;
   }

ff443b8d5865f0b4f618f4e3babec02675f4b1aa
k2o <11086104+ksugawara61@users.noreply.github.com>
2026-02-08 21:38:06 +0900

    fix: signouturl

--- apps/extension/src/shared/providers/app-provider/index.tsx
+++ apps/extension/src/shared/providers/app-provider/index.tsx
@@ -7,7 +7,10 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY ?? "";
 
 export const AppProvider: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      afterSignOutUrl="/sidepanel/index.html"
+    >
       <AuthGuard>
         <ApolloProvider>{children}</ApolloProvider>
       </AuthGuard>

6327a4a4361aea2fc51993bc213e00594905c307
k2o <11086104+ksugawara61@users.noreply.github.com>
2026-02-09 18:20:45 +0900

    feat: popup で redirect する処理を追加 (#104)

--- apps/extension/src/pages/popup/main.tsx
+++ apps/extension/src/pages/popup/main.tsx
@@ -7,7 +7,7 @@ import { AppProvider } from "../../shared/providers/app-provider";
 // biome-ignore lint/style/noNonNullAssertion: root element is guaranteed to exist
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AppProvider>
+    <AppProvider path="/popup/index.html">
       <Popup />
     </AppProvider>
   </React.StrictMode>,

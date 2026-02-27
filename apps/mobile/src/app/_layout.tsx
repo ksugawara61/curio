959514857da46454dd271389340c05632980fdcf
k2o <11086104+ksugawara61@users.noreply.github.com>
2026-02-28 01:41:22 +0900

    feat: GraphQL Query でリクエストを送ってUI Jsonを取得

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -1,5 +1,4 @@
-import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
-import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import { useAuth } from "@clerk/clerk-expo";
 import { Stack } from "expo-router";
 import { AppProvider } from "../shared/app-provider";
 

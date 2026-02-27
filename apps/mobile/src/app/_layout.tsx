873e52b57572a1b6cc3107e1e1e2feda62894f59
k2o <11086104+ksugawara61@users.noreply.github.com>
2026-02-28 01:25:25 +0900

    feat: clerk でログインする機構を仮実装

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -1,5 +1,29 @@
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Stack } from "expo-router";
 
+const NavigationLayout = () => {
+  const { isSignedIn = false } = useAuth();
+
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Protected guard={!isSignedIn}>
+        <Stack.Screen name="(auth)/sign-in" />
+        <Stack.Screen name="(auth)/sign-up" />
+      </Stack.Protected>
+      <Stack.Protected guard={isSignedIn}>
+        <Stack.Screen name="(home)/index" />
+      </Stack.Protected>
+    </Stack>
+  );
+};
+
+const PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY ?? "";
+
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} tokenCache={tokenCache}>
+      <NavigationLayout />
+    </ClerkProvider>
+  );
 }
